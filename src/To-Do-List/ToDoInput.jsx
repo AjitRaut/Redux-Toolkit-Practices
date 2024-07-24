390c969b63992e48b831a47d33be350191c81732
Ajit raut <ajitraut9561@gmail.com>
2024-07-24 20:27:57 +0530

    State successFully Added

--- src/To-Do-List/ToDoInput.jsx
+++ src/To-Do-List/ToDoInput.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Add_Todo } from "./ToDoSlice";
 
 const ToDoInput = () => {
+const [input , setinput] = useState("");
+
   const todo = useSelector((store)=>store.todo.todos)
   console.log(todo)
 
   const dispatch = useDispatch();
 
-  const handleForm =()=>{
+  const handleForm =(e)=>{
     e.preventDefault();
   }
 
   const handleTodo =()=>{
-    dispatch(Add_Todo())
+    if (input.trim() !== "") {
+      dispatch(Add_Todo(input));
+      setinput("");
+    }
+   
   }
   return (
     <div className="flex justify-around ">
       <form onSubmit={handleForm}>
         <div className="border-solid border border-gray-500 rounded-sm w-full ">
-          <input className="outline-none px-2" />
+          <input value={input} onChange={(e)=>setinput(e.target.value)} className="outline-none px-2" />
           <button className="bg-sky-700 p-2 m-2 rounded-lg text-xl  font-bold" onClick={handleTodo}>
             Add
           </button>
