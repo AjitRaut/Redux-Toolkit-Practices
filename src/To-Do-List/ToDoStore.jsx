dde55369021a617c73cdbcc0a7a93a213e5a5ac3
Ajit raut <ajitraut9561@gmail.com>
2024-07-24 19:25:56 +0530

    Redux Practice

--- src/To-Do-List/ToDoStore.jsx
+++ src/To-Do-List/ToDoStore.jsx
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import React from 'react'
+import todoslice from "./ToDoSlice"
 
 const ToDoStore = configureStore({
-    
+    reducer : todoslice,
 })
 
 export default ToDoStore
