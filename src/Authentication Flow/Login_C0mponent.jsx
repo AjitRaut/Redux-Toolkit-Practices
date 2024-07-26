a809e4a4983afadf99a6714e28936014590257b3
Ajit raut <ajitraut9561@gmail.com>
2024-07-26 17:22:26 +0530

    Login Component added

--- src/Authentication Flow/Login_C0mponent.jsx
+++ src/Authentication Flow/Login_C0mponent.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login_C0mponent = () => {
+const [username , setusername] = useState("");
+const [password , setpassword] = useState("");
+
   return (
     <>
      <div>
         <h1>Login Component</h1>
         <form>
+            <p>Username</p>
             <input value={} />
+            <p>Password</p>
+            <input type="password"  />
         </form>
         </div> 
     </>
