f61ac159e428063ce19661d96369986b4d559c76
Ajit raut <ajitraut9561@gmail.com>
2024-07-26 17:37:25 +0530

    Login Component added

--- src/Authentication Flow/Login_C0mponent.jsx
+++ src/Authentication Flow/Login_C0mponent.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginFailure, loginSuccess } from './Authentication_Flow_Slice';
 
 const Login_C0mponent = () => {
 const [username , setusername] = useState("");
 const [password , setpassword] = useState("");
+const dispatch = useDispatch();
+
+const handleonsumbit =(e)=>{
+    e.preventDefault();
+}
+
+const handlesumbit =()=>{
+    if(username === "ajit" && password === "pass")
+    {
+        dispatch(loginSuccess({username}))
+    }else{
+        dispatch(loginFailure("invalid credentiols"))
+    }
+}
+
 
   return (
     <>
      <div>
         <h1>Login Component</h1>
-        <form>
+        <form onSubmit={handleonsumbit}>
             <p>Username</p>
-            <input value={} />
+            <input value={username} onChange={(e)=>setusername(e.target.value)} />
+
             <p>Password</p>
-            <input type="password"  />
+            <input type="password" value={password} onChange={(e)=>setpassword(e.target.value)}  />
+            <button onClick={handlesumbit}>Submit</button>
         </form>
         </div> 
     </>
